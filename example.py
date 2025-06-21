053f1821a459f02ec538d2a4c66e688464e723ce
中村邦臣 <ybpgx160@icloud.com>
2025-06-22 08:01:09 +0900

    ハッカソンテスト

---
+++ example.py
@@ -0,0 +1,20 @@
+print("hello world")
+
+def greet(hello):
+    print(hello)
+greet("こんにちは")
+
+def print_name(name):
+    print(f"私の名前は{name}です")
+print_name("太郎")
+
+def get_greet(namae):
+    return f"おはようございます {namae}さん"
+
+print(get_greet("花子"))
+
+def add(a,b):
+    return a + b
+siki = add(10,10)
+print(siki)
+
